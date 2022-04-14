97c0566fe8f4a9ed15f85b5b8c9d6ccb68176fea
dash <jeremy.estienne@epitech.eu>
2022-04-14 10:08:28 +0200

    [feature] bootstrap

---
+++ bootstrap.py
@@ -0,0 +1,77 @@
+import numpy as np
+import gym
+import random
+env = gym.make("FrozenLake-v0")
+
+action_size = env.action_space.n
+state_size = env.observation_space.n
+
+qtable = np.zeros((state_size, action_size))
+print(qtable)
+
+total_episodes = 20000       
+learning_rate = 0.7          
+max_steps = 99             
+gamma = 0.95                
+
+
+epsilon = 1.0                 
+max_epsilon = 1.0             
+min_epsilon = 0.01           
+decay_rate = 0.005       
+
+rewards = []
+for episode in range(total_episodes):
+    state = env.reset()
+    step = 0
+    done = False
+    total_rewards = 0
+    
+    for step in range(max_steps):
+        exp_exp_tradeoff = random.uniform(0, 1)
+        if exp_exp_tradeoff > epsilon:
+            action = np.argmax(qtable[state,:])
+        else:
+            action = env.action_space.sample()
+        new_state, reward, done, info = env.step(action)
+        qtable[state, action] = qtable[state, action] + learning_rate * (reward + gamma * np.max(qtable[new_state, :]) - qtable[state, action])
+        total_rewards += reward
+        state = new_state
+        
+        if done == True: 
+            break
+ 
+    epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode) 
+    rewards.append(total_rewards)
+    
+
+print ("Score: " +  str(sum(rewards)/total_episodes))
+print(qtable)
+
+env.reset()
+
+for episode in range(20):
+    state = env.reset()
+    step = 0
+    done = False
+    print("____________")
+    print("EPISODE ", episode)
+
+    for step in range(max_steps):
+        
+        action = np.argmax(qtable[state,:])
+        
+        new_state, reward, done, info = env.step(action)
+        
+        if done:
+            env.render()
+            if new_state == 15:
+                print("c'est bon !")
+            else:
+                print("la douche froide")
+            
+            print("etapes", step)
+            
+            break
+        state = new_state
+env.close()
