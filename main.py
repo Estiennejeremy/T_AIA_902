85f819f157532a6b6dca23be245c57430eda3aa9
dash <jeremy.estienne@epitech.eu>
2022-05-19 15:12:37 +0200

    [feature] set as api brutforce and qlearning

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from typing import Optional
 
 from fastapi import FastAPI
-
+from qlearning import qlearning
+from force import force
 app = FastAPI()
+
+
+class Item(BaseModel):
+    gamma: float
+    alpha: float
+    epsilon: float
+    episodes: int
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/bruteforce")
+def qLearningRoute(item: Item):
+    print(item.trajet)
+    return qlearning(item.episodes, item.gamma, item.alpha, item.epsilon)
+
+
+@app.post("/qlearning")
+def qLearningRoute(item: Item):
+    print(item.trajet)
+    return qlearning(item.episodes, item.gamma, item.alpha, item.epsilon)
