1a10df93d235ac7bd33eae2002ce04beb199a394
dash <jeremy.estienne@epitech.eu>
2022-06-16 09:12:13 +0200

    [fix] remove render for server

--- force.py
+++ force.py
@@ -4,10 +4,10 @@ env = gym.make("Taxi-v3").env
 
 
 def force():
-    env.render()
+   # env.render()
 
     env.reset()  # reset environment to a new, random state
-    env.render()
+   # env.render()
 
     print("Action Space {}".format(env.action_space))
     print("State Space {}".format(env.observation_space))
